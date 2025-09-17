552c12447491eca1a68b21dd96c2faa1dfb96d5e
mayesha <soumyrhmn234@gmail.com>
2025-09-17 09:53:37 +0600

    create loginWithGoogle route

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -4,6 +4,7 @@ import { AuthController } from './auth.controller';
 const router = express.Router();
 
 router.get("/login", AuthController.loginWithEmailAndPassword)
+router.get("/google", AuthController.loginWithGoogle)
 
 export const authRouter = router
 
