f8c816704dea0e67b6384838f6d2e4c8acc03d43
mayesha <soumyrhmn234@gmail.com>
2025-09-17 09:31:03 +0600

    update auth/login service

--- src/modules/auth/auth.services.ts
+++ src/modules/auth/auth.services.ts
@@ -4,7 +4,18 @@ const loginWithEmailAndPassword = async ({email, password}: {email: string, pass
     console.log({email, password}) 
     const user = await prisma.user.findUnique({
         where:{email}
-    })   
+    }) 
+    
+    if(!user){
+        throw new Error("User does not exist.")
+    }
+
+    if(password === user.password){
+        return user
+    }
+    else{
+        throw new Error("Password does not match.")
+    }
 }
 
 export const AuthServices = {
