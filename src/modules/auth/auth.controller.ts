c6314aff283a7c37ed2f9002e736720c465bcdd2
mayesha <soumyrhmn234@gmail.com>
2025-09-17 09:51:50 +0600

    create loginWithGoogle controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -18,7 +18,25 @@ const loginWithEmailAndPassword = async (req: Request, res: Response, next: Next
     });
   }
 };
+const loginWithGoogle = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await AuthServices.loginWithGoogle(req.body);
+    res.status(201).json({
+      success: true,
+      data: result,
+      message: "Logged In successfully",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: "Failed to login",
+      error: err instanceof Error ? err.message : err,
+    });
+  }
+};
 
 export const AuthController = {
-    loginWithEmailAndPassword
+    loginWithEmailAndPassword,
+    loginWithGoogle
 }
